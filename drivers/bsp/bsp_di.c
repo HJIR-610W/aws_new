d1bca358fab07b30e504c1f518ffe788ccf631ac
wth <wonth0902@gmail.com>
2025-07-29 14:31:36 +0900

    리팩토링

--- drivers/bsp/bsp_di.c
+++ drivers/bsp/bsp_di.c
@@ -7,7 +7,6 @@
 #include "util_memory.h"
 #include "pcb_define.h"
 
-
 typedef struct bsp_di_inst_s
 {
   GPIO_InitTypeDef init;
@@ -15,9 +14,9 @@ typedef struct bsp_di_inst_s
   bool opened;
 } bsp_di_inst_t;
 
- bsp_di_inst_t di_inst[BSP_DI_MCU_MAX] = {
+bsp_di_inst_t di_inst[BSP_DI_MCU_MAX] = {
 
-    [BSP_DI_0_ADC_RDY_ONLY] = {.init = {.Pin = DI_SPI2_DRDY_Pin, .Pull = GPIO_PULLUP},
+    [BSP_DI_0_ADC_RDY] = {.init = {.Pin = DI_SPI2_DRDY_Pin, .Pull = GPIO_PULLUP},
                                .port = DI_SPI2_DRDY_GPIO_Port},
     [BSP_DI_USER_BTN] = {.init = {.Pin = DI_SW_SYS_Pin, .Pull = GPIO_PULLUP},
                          .port = DI_SW_SYS_GPIO_Port},
@@ -28,7 +27,7 @@ typedef struct bsp_di_inst_s
     [BSP_DI_RAIN_HALL_ERR] = {.init = {.Pin = DI_RAIN_HALL_ERR_Pin, .Pull = GPIO_PULLUP},
                               .port = DI_RAIN_HALL_ERR_GPIO_Port},
     [BSP_DI_RAIN_DETECT_A] = {.init = {.Pin = DI_RAIN_DETECT_Pin, .Pull = GPIO_PULLUP},
-                               .port = DI_RAIN_DETECT_GPIO_Port},
+                              .port = DI_RAIN_DETECT_GPIO_Port},
     [BSP_DI_QUAD_UARTA_1] = {.init = {.Pin = DI_EX_UART_INT1_Pin, .Pull = GPIO_PULLUP},
                              .port = DI_EX_UART_INT1_GPIO_Port},
     [BSP_DI_QUAD_UARTB_2] = {.init = {.Pin = DI_EX_UART_INT2_Pin, .Pull = GPIO_PULLUP},
@@ -47,21 +46,24 @@ typedef struct bsp_di_inst_s
                              .port = DI_EX_UART_INT8_GPIO_Port},
 
     [BSP_DI_WAKE_UP] = {.init = {.Pin = DI_WAKE_UP_Pin, .Pull = GPIO_PULLUP},
-                     .port = DI_WAKE_UP_GPIO_Port},
+                        .port = DI_WAKE_UP_GPIO_Port},
 
     [BSP_DI_BOOT1] = {.init = {.Pin = DI_BOOT1_Pin, .Pull = GPIO_PULLUP},
-                   .port = DI_BOOT1_GPIO_Port},
+                      .port = DI_BOOT1_GPIO_Port},
     [BSP_DI_USB_POWER_FAIL] = {.init = {.Pin = DI_USB_OTG_PWR_FAIL_Pin, .Pull = GPIO_PULLUP},
-                            .port = DI_USB_OTG_PWR_FAIL_GPIO_Port},
+                               .port = DI_USB_OTG_PWR_FAIL_GPIO_Port},
 
     [BSP_DI_SD_IN] = {.init = {.Pin = DI_SDIO_DETECT_Pin, .Pull = GPIO_PULLUP},
-                   .port = DI_SDIO_DETECT_GPIO_Port},
+                      .port = DI_SDIO_DETECT_GPIO_Port},
 
     [BSP_DI_IO_INT] = {.init = {.Pin = DI_INT_D_IO_Pin, .Pull = GPIO_PULLUP},
-                    .port = DI_INT_D_IO_GPIO_Port},
+                       .port = DI_INT_D_IO_GPIO_Port},
 
     [BSP_DI_RTC_INT] = {.init = {.Pin = DI_INT_RTC_Pin, .Pull = GPIO_PULLUP},
-                     .port = DI_INT_RTC_GPIO_Port},
+                        .port = DI_INT_RTC_GPIO_Port},
+
+    [BSP_DI_HART_CD] = {.init = {.Pin = DI_CD_H_Pin, .Pull = GPIO_PULLUP},
+                             .port = DI_CD_H_GPIO_Port},
 };
 
 void bsp_di_gpio_init(int di_number)
@@ -86,7 +88,7 @@ void bsp_di_init(void)
     di_inst[di_num].opened= true;
       switch (di_num)
       {
-        case BSP_DI_0_ADC_RDY_ONLY:
+        case BSP_DI_0_ADC_RDY:
         case BSP_DI_USER_BTN:
         case BSP_DI_RAIN_REED:
         case BSP_DI_RAIN_HALL:
@@ -122,7 +124,7 @@ int32_t bsp_di_read(int32_t di_number)
 {
   switch (di_number)
   {
-    case BSP_DI_0_ADC_RDY_ONLY:
+    case BSP_DI_0_ADC_RDY:
     case BSP_DI_USER_BTN:
     case BSP_DI_RAIN_REED:
     case BSP_DI_RAIN_HALL:
@@ -282,7 +284,7 @@ void bsp_di_set_interrupt(int di_number, di_isr_set_cfg_t *isr_cfg)
   uint16_t pin;
   switch (di_number)
   {
-    case BSP_DI_0_ADC_RDY_ONLY:
+    case BSP_DI_0_ADC_RDY:
     case BSP_DI_USER_BTN:
     case BSP_DI_RAIN_REED:
     case BSP_DI_RAIN_HALL:
--- drivers/bsp/bsp_di.h
+++ drivers/bsp/bsp_di.h
@@ -9,39 +9,35 @@
 
 #include "driver_di_def.h"
 
-#define BSP_DI_0_ADC_RDY_ONLY 0
-#define BSP_DI_HART_CD_ONLY   1
-#define BSP_DI_USER_BTN       2
-#define BSP_DI_RAIN_REED      3
-#define BSP_DI_RAIN_HALL      4
-#define BSP_DI_RAIN_HALL_ERR  5
-#define BSP_DI_RAIN_DETECT_A  6
 
-#define BSP_DI_QUAD_UARTA_1 7
-#define BSP_DI_QUAD_UARTB_2 8
-#define BSP_DI_QUAD_UARTC_3 9
-#define BSP_DI_QUAD_UARTD_4 10
-#define BSP_DI_QUAD_UARTA_5 11
-#define BSP_DI_QUAD_UARTB_6 12
-#define BSP_DI_QUAD_UARTC_7 13
-#define BSP_DI_QUAD_UARTD_8 14
-
-#define BSP_DI_0 15
-#define BSP_DI_1 16
-#define BSP_DI_2 17
-#define BSP_DI_3 18
-#define BSP_DI_4 19
-#define BSP_DI_5 20
-
-#define BSP_DI_WAKE_UP        21
-#define BSP_DI_BOOT1          22 
-#define BSP_DI_HART_CD        23
-#define BSP_DI_RTC_INT        24
-#define BSP_DI_SD_IN          25
-#define BSP_DI_USB_POWER_FAIL 26
-#define BSP_DI_IO_INT         27
-
-#define BSP_DI_MCU_MAX        28
+#define BSP_DI_USER_BTN        0
+#define BSP_DI_RAIN_REED       1
+#define BSP_DI_RAIN_HALL       2
+#define BSP_DI_RAIN_HALL_ERR   3
+#define BSP_DI_RAIN_DETECT_A   4
+#define BSP_DI_QUAD_UARTA_1    5
+#define BSP_DI_QUAD_UARTB_2    6
+#define BSP_DI_QUAD_UARTC_3    7
+#define BSP_DI_QUAD_UARTD_4    8
+#define BSP_DI_QUAD_UARTA_5    9
+#define BSP_DI_QUAD_UARTB_6   10
+#define BSP_DI_QUAD_UARTC_7   11
+#define BSP_DI_QUAD_UARTD_8   12
+#define BSP_DI_WAKE_UP        13 // 미사용
+#define BSP_DI_BOOT1          14 // 미사용
+#define BSP_DI_HART_CD   15
+#define BSP_DI_RTC_INT        16
+#define BSP_DI_0_ADC_RDY 17
+#define BSP_DI_IO_INT         18
+#define BSP_DI_SD_IN          19
+#define BSP_DI_USB_POWER_FAIL 20
+#define BSP_DI_0 21
+#define BSP_DI_1 22
+#define BSP_DI_2 23
+#define BSP_DI_3 24
+#define BSP_DI_4 25
+#define BSP_DI_5 26
+#define BSP_DI_MCU_MAX 27
 
 
 
